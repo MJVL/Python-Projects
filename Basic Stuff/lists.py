26bf121a36c721bb0630376048db3c4e7db6a8e2
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-20 13:28:12 -0500

    Add lists.py

---
+++ Basic Stuff/lists.py
@@ -0,0 +1,23 @@
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+print(colors)
+
+colors.append('pink')
+
+print(colors)
+
+numbers = []
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+print(numbers)
+
+numbers.insert(0,0)
+
+print(numbers)
+
+del(numbers[0])
+
+print(numbers)
