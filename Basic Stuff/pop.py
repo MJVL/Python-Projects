88a2400c1bbad78a7eb176c9b37be7ca3e6e485a
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-20 13:33:11 -0500

    Add pop.py

---
+++ Basic Stuff/pop.py
@@ -0,0 +1,14 @@
+names = ["Bob", "Sarah", "Emily", "Joe"]
+print(names)
+
+popped_name = names.pop();
+print(names)
+print(popped_name)
+
+first_person = names.pop(0)
+print(names)
+print(first_person)
+
+fired_person = "Emily"
+names.remove(fired_person)
+print(names)
