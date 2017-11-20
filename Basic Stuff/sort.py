0b58c91b4f7c6af2754ca73399b2ff825d604041
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-20 13:37:43 -0500

    Add sort.py

---
+++ Basic Stuff/sort.py
@@ -0,0 +1,14 @@
+years = ["2018", "2015", "2017", "2016"]
+years.sort()
+print(years)
+years.sort(reverse=True)
+print(years)
+years.reverse()
+print(years)
+
+#Temp Sorting
+subjects = ["Math", "Science", "English", "History"]
+print(subjects)
+print(sorted(subjects))
+print(sorted(subjects,reverse=True))
+print(subjects)
