3c61b885f6a3ce7b8ed9f433cff3b8f0cd61333a
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-20 13:03:02 -0500

    Add string_functions.py

---
+++ Basic Stuff/string_functions.py
@@ -0,0 +1,16 @@
+name = "michael van leeuwen"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "michael"
+last_name = "van leeuwen"
+full_name = first_name + " " + last_name
+
+print("Hello, " + full_name.title() + ".")
+
+print("\tTab and New Line\n")
+
+intentional_space = "python "
+print(intentional_space)
+print(intentional_space.rstrip())
