20a9ac67e3b44bc7f5305f1753b06c4323738467
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-21 15:44:11 -0500

    Add loops.py

---
+++ Basic Stuff/loops.py
@@ -0,0 +1,14 @@
+primes = [3, 5, 7, 11, 13]
+sum = 0
+for prime in primes:
+    sum += prime
+    print("Current Prime: " + str(prime))
+print("Sum of Primes: " + str(sum))
+for num in range(1,6):
+    print(num)
+somenums = list(range(1,11))
+print(somenums)
+cubes = []
+for i in range(1,11):
+    cubes.append(i**3)
+print(cubes)
