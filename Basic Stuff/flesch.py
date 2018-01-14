4f61413aa74e91193bc54e579f796936f0c0fb70
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-01-14 11:22:47 -0500

    Convert lists to tuples

--- Basic Stuff/flesch.py
+++ Basic Stuff/flesch.py
@@ -1,5 +1,5 @@
-Flesch_Indexs = [91,81,71,61,51,31,0]
-Flesch_Levels = ["5th Grade","6th Grade","7th Grade","8th and 9th Grade","10th to 12th Grade","College Student","College Graduate"];
+Flesch_Indexs = (91,81,71,61,51,31,0)
+Flesch_Levels = ("5th Grade","6th Grade","7th Grade","8th and 9th Grade","10th to 12th Grade","College Student","College Graduate");
 
 Flesch_Score = int(input("Enter the text's flesch score: "))
 
