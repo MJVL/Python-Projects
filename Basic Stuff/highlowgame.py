8c21951e5831e7e29b31dd4081dee751165331fe
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-01-14 11:13:35 -0500

    Add highlowgame.py

---
+++ Basic Stuff/highlowgame.py
@@ -0,0 +1,16 @@
+import random
+
+randNum = random.randint(1,100)
+numGuesses = 0
+guess = -1
+
+while guess != randNum:
+    print('Enter your guess:')
+    guess = int(input())
+    numGuesses += 1
+    if guess < randNum:
+        print('Too low.')
+    elif guess > randNum:
+        print('Too high.')
+    else:
+        print('You guessed it, the correct number was ' + str(randNum) + '. It took you ' + str(numGuesses) + 'guesses.')
