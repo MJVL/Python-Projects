727035307525197e29296510dd365b2f094cbe2e
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-03-10 15:44:46 -0500

    Update bitwise_operators.py

--- Basic Stuff/bitwise_operators.py
+++ Basic Stuff/bitwise_operators.py
@@ -4,4 +4,22 @@ print(0b11 * 0b11)
 
 print(bin(10)),
 print(hex(10)),
-print()
+print(oct(10))
+
+for i in range(2,6):
+	print(bin(i))
+
+shift_right = 0b1100
+shift_left = 0b1
+
+shift_right = shift_right >> 2
+shift_left = shift_left << 2
+
+print(bin(shift_right))
+print(bin(shift_left))
+
+
+print(bin(0b1110 & 0b101))
+print(bin(0b1110 | 0b101))
+print(bin(0b1110 ^ 0b101))
+print(bin(~0b110))
