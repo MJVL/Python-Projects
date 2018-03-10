3ec7ea9b92dafd2a829bb5fe34fd9e0f58a50ebe
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-03-10 15:54:24 -0500

    Use bit masks

--- Basic Stuff/bitwise_operators.py
+++ Basic Stuff/bitwise_operators.py
@@ -23,3 +23,31 @@ print(bin(0b1110 & 0b101))
 print(bin(0b1110 | 0b101))
 print(bin(0b1110 ^ 0b101))
 print(bin(~0b110))
+
+# Check if bit is on at position 4
+def check_bit4(input):
+	mask = 0b1000
+	desired = input & mask
+	if desired > 0:
+		return "on"
+	else:
+		return "off"
+
+
+print(check_bit4(0b1))
+print(check_bit4(0b11011))
+print(check_bit4(0b1010))
+
+# Turn bit on at third position
+a = 0b10111011
+mask = 0b100
+print(bin(a | mask))
+
+# Flip all bits in a
+a = 0b11101110
+mask = 0b11111111
+print(bin(a ^ mask))
+
+# Flip nth bit
+def flip_bit(number, n):
+	return bin(number ^ ob1 << (n-1))
